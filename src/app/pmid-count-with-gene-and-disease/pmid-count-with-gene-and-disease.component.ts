75ae95a3cd04d05479fe144ad568dfe75c12ecce
gautammukherjee <gmukherjee@bioxcel.com>
2023-07-05 11:39:55 +0530

    changes

--- src/app/pmid-count-with-gene-and-disease/pmid-count-with-gene-and-disease.component.ts
+++ src/app/pmid-count-with-gene-and-disease/pmid-count-with-gene-and-disease.component.ts
@@ -1,10 +1,111 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { RelationDistributionService } from '../services/relation-distribution.service';
+import { GlobalVariableService } from 'src/app/services/common/global-variable.service';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-pmid-count-with-gene-and-disease',
   templateUrl: './pmid-count-with-gene-and-disease.component.html',
   styleUrls: ['./pmid-count-with-gene-and-disease.component.scss']
 })
-export class PmidCountWithGeneAndDiseaseComponent {
+export class PmidCountWithGeneAndDiseaseComponent implements OnInit {
+  data: any;
+  errorMsg: string | undefined;
+  graphLoader: boolean = true;
+  private filterParams: any;
+  highcharts = Highcharts;
+  chartOptions: any;
+
+  constructor(
+    private _router: Router,
+    private _activatedRoute: ActivatedRoute,
+    private _RDS: RelationDistributionService,
+    private globalVariableService: GlobalVariableService,
+  ) { }
+
+  ngOnInit(): void {
+    this.filterParams = this.globalVariableService.getFilterParams();
+    this._RDS.details_of_association_type(this.filterParams).subscribe(
+      (response: any) => {
+        this.data = response.nodeSelectsRecords;
+        this.drawAreaChart();
+      },
+      (error: any) => {
+        console.error(error)
+        this.errorMsg = error;
+      }
+    );
+  }
+
+  drawAreaChart() {
+    console.log("In drawAreaChart");
+    console.log(this.data);
+
+    this.chartOptions = {
+      chart: {
+        type: 'areaspline'
+      },
+      title: {
+        text: 'Average fruit consumption during one week'
+      },
+      subtitle: {
+        style: {
+          position: 'absolute',
+          right: '0px',
+          bottom: '10px'
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'left',
+        verticalAlign: 'top',
+        x: 150,
+        y: 100,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: '#FFFFFF'
+      },
+      xAxis: {
+        categories: ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
+          'Friday', 'Saturday', 'Sunday']
+      },
+      yAxis: {
+        title: {
+          text: 'Fruit units'
+        }
+      },
+      tooltip: {
+        shared: true,
+        valueSuffix: ' units'
+      },
+      credits: {
+        enabled: false
+      },
+      plotOptions: {
+        areaspline: {
+          fillOpacity: 0.5
+        }
+      },
+      series: [
+        {
+          name: 'John',
+          data: [3, 4, 3, 5, 4, 10, 12]
+        },
+        {
+          name: 'Jane',
+          data: [1, 3, 4, 3, 3, 5, 4]
+        }
+      ]
+
+
+    };
+
+  };
+
+
+
 
 }
+
+
