63ebdb79b944ad728e92ca43e0cf44808c214c06
bioxcel <bioxcel@EZB-IN-L-48.ezb.com>
2024-01-23 13:16:07 +0530

    cl changes

--- src/app/event-description/event-description.component.ts
+++ src/app/event-description/event-description.component.ts
@@ -81,7 +81,7 @@ export class EventDescriptionComponent implements OnInit {
   notEmptyPost: boolean = true;
   notscrolly: boolean = true;
   currentPage: number = 1;
-  itemsPerPage: number = 5000;
+  itemsPerPage: number = 10000;
   public isloading: boolean = false;
   public loadingArticleSaved: boolean = false;
   loaderEvidence = false;
