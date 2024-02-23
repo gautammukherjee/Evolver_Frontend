ecfb19e921c4bdfad6974462183dfc447d7b29f3
bioxcel <bioxcel@EZB-IN-L-48.ezb.com>
2024-02-23 15:24:01 +0530

    frontend changes

--- src/app/ct/ct_investigator_rels/ct_investigator_rels.component.ts
+++ src/app/ct/ct_investigator_rels/ct_investigator_rels.component.ts
@@ -29,6 +29,7 @@ export class CTInvestigatorRelsComponent implements OnInit {
   InvestigatorRelDetailsData: any = [];
   hideCardBody: boolean = true;
   itemsPerPage: number = 2;
+  notEmptyData: boolean = true;
 
   constructor(
     private globalVariableService: GlobalVariableService,
@@ -52,37 +53,47 @@ export class CTInvestigatorRelsComponent implements OnInit {
   getCTDataInvestigatorWithRels() {
     this.filterParams = this.globalVariableService.getFilterParams({ "offSetValue": 0, "limitValue": this.itemsPerPage });
     console.log("params in CT in rels: ", this.filterParams);
-
+ 
     if (this.filterParams.source_node != undefined) {
       // $('.overlay').fadeOut(500);
       this.loadingCTInvestRelData = true;
 
       //console.log("filterparams: ", _filterParams);
-      this.nodeSelectsService.getCTTrialInvestRels(this.filterParams).subscribe(
+      this.nodeSelectsService.getCTTrialInvestRels_new(this.filterParams).subscribe(
         data => {
           //console.log("data: ", data);
           this.result = data;
-          this.InvestigatorRelData = this.result.CTRelsDATA;
-          console.log("CT Rels data LOAD: ", this.InvestigatorRelData);
+          this.InvestigatorRelData = this.result.CTUNIQUEDATA;
+          console.log("CT Unique data LOAD: ", this.InvestigatorRelData);
 
           // this.loadingCTInvestRelData = false;
           this.InvestigatorRelDetailsData = [];
           this.InvestigatorRelData.forEach((event: any) => {
             var temps: any = {};
-            temps["node_id"] = event.node_id;
-            temps["disease_name"] = event.disease_name;
             temps["ct_id"] = event.ct_id;
+            temps["has_expanded_access"] = event.has_expanded_access;
             temps["nct_id"] = event.nct_id;
-            temps["investigator_id"] = event.investigator_id;
-            temps["investigator_name"] = event.investigator_name;
-            temps["affiliation"] = event.affiliation;
-            temps["investigator_role_id"] = event.investigator_role_id;
-            temps["role"] = event.role;
-            temps["country_id"] = event.country_id;
-            temps["country_name"] = event.country_name;
-            temps["pmids"] = event.pmids;
+            temps["node_id"] = event.node_id;
+            temps["org_study_id"] = event.org_study_id;
+            temps["secondary_study_id"] = event.secondary_study_id;
+            temps["title"] = event.title;
+            temps["overall_status"] = event.overall_status;
+            temps["phase_name"] = event.phase_name;
+            temps["minimum_age"] = event.minimum_age;
+            temps["maximum_age"] = event.maximum_age;
+            temps["healthy_volunteers"] = event.healthy_volunteers;
+            temps["verification_date"] = event.verification_date;
+            temps["last_update_submitted"] = event.last_update_submitted;
+            temps["last_update_submitted_qc"] = event.last_update_submitted_qc;
+            temps["study_first_posted"] = event.study_first_posted;
+            temps["study_first_submitted"] = event.study_first_submitted;
+            temps["study_first_submitted_qc"] = event.study_first_submitted_qc;
+            temps["study_type"] = event.study_type;
+            temps["gender"] = event.gender;
+            temps["trial_design"] = event.trial_design;
+            temps["phase_id"] = event.phase_id;
 
-            this.InvestigatorRelDetailsData.push(temps);            
+            this.InvestigatorRelDetailsData.push(temps);
           });
           this.bootstrapTableChartInRels();
         },
@@ -92,8 +103,11 @@ export class CTInvestigatorRelsComponent implements OnInit {
         },
         () => {
           this.loadingCTInvestRelData = false;
+          this.notEmptyData = false;
         }
       );
+    }else{
+      this.notEmptyData = true;
     }
 
   }
